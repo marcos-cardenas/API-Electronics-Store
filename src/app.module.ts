d464ac20391810e6fb79281cd930d2933174b647
marcos-cardenas <marcoscardenashernandez04@gmail.com>
2026-01-01 16:37:44 -0600

    Configuracion BD y API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CommonModule,
+    SeedModule,
+  ],
 })
 export class AppModule {}
