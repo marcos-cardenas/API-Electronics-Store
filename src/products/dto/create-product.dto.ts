5d2caa9cd567878e60dbb649f456fd7b4c4b47f5
marcos-cardenas <marcoscardenashernandez04@gmail.com>
2026-01-07 18:30:24 -0600

    Relacion y Seguridad API

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,9 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Type } from 'class-transformer';
 import {
   IsArray,
   IsInt,
   IsNotEmpty,
+  IsNotEmptyObject,
   IsNumber,
   IsOptional,
   IsPositive,
@@ -11,7 +11,9 @@ import {
   Matches,
   Min,
   MinLength,
+  ValidateNested,
 } from 'class-validator';
+import { CreateTechnicalSpecDto } from 'src/technical-specs/dto/create-technical-spec.dto';
 
 export class CreateProductDto {
   @Matches(/^[A-Z][A-Z0-9-]{4,29}$/)
@@ -32,4 +34,13 @@ export class CreateProductDto {
   @IsArray()
   @IsString({ each: true })
   tags: string[];
+  @IsNotEmpty()
+  @IsInt()
+  @IsPositive()
+  @Type(() => Number)
+  brandId: number;
+  @IsNotEmptyObject()
+  @ValidateNested()
+  @Type(() => CreateTechnicalSpecDto)
+  technicalSpec: CreateTechnicalSpecDto;
 }
